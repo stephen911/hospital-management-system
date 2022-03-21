9d5f972e9a2434bab357ea8e22cf96596c148315
stephen911 <stephendappah1@gmail.com>
2022-03-21 16:29:01 +0000

    'new'

--- update.py
+++ update.py
@@ -29,4 +29,5 @@ class Application:
                                                                                                                                         self.input = self.namenet.get()
                                                                                                                                                 ~ execute sql 
 
-                                                                                                                                                
+                                                                                                                                                        sql = "SELECT * FROM appointments WHERE name LIKE ?"
+                                                                                                                                                        
