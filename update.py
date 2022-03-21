722aeac1bed89e66cd072a0c322299cde687eb00
stephen911 <stephendappah1@gmail.com>
2022-03-21 16:26:25 +0000

    'new'

--- update.py
+++ update.py
@@ -10,4 +10,5 @@ class Application:
         def __init__(self, master):
                     self.master = master
                             ~ heading label
-                            
+                                    self.heading = Label(master, text="Update Appointments",  fg='steelblue', font=('arial 40 bold'))
+                                    
