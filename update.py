b3d88fde1cb426839e5643a2ce3509cccb2ecf3e
stephen911 <stephendappah1@gmail.com>
2022-03-21 16:29:18 +0000

    'new'

--- update.py
+++ update.py
@@ -31,4 +31,5 @@ class Application:
 
                                                                                                                                                         sql = "SELECT * FROM appointments WHERE name LIKE ?"
                                                                                                                                                                 self.res = c.execute(sql, (self.input,))
-                                                                                                                                                                
+                                                                                                                                                                        for self.row in self.res:
+                                                                                                                                                                            
