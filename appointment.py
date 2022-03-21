fb5ae610fab7577973a01e84968999939628b225
stephen911 <stephendappah1@gmail.com>
2022-03-21 15:45:10 +0000

    'new'

--- appointment.py
+++ appointment.py
@@ -16,4 +16,5 @@ class Application:
                     self.master = master
 
                             ~ creating the frames in the master
-                            
+                                    self.left = Frame(master, width=800, height=720, bg='lightgreen')
+                                    
