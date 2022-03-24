90a9d30c07712cddad5327a0bb0d1cd015cf4e07
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:18:40 +0000

    'new'

--- display.py
+++ display.py
@@ -120,6 +120,7 @@
 ~ 3. Better data structure - I believe the structure I have used to keep the state of
 ~ the board (list of a list) may be slowing down accessing its elements or assigning
 ~ its elements. Improvement in efficiency of my code by changing data structures may
+~ potentially improve the speed at which my AI makes its move.
 
 
 
