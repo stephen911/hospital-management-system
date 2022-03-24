a6685f723debfff0f8db026264d764c130617bfc
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:13:27 +0000

    'new'

--- display.py
+++ display.py
@@ -77,6 +77,7 @@
 ~ guarantee better results. This can happen because white and black area against
 ~ one another. White plays what is best for it and black plays what is best for it,
 ~ so it would make sense for white to ignore any portion of the tree where black
+~ has a clear upperhand that it can choose to play.
 
 
 
