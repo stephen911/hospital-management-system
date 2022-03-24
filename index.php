ffeb2cb3206c606453290d531a3544978a02866d
stephen911 <stephendappah1@gmail.com>
2022-03-24 21:02:26 +0000

    'new'

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <!DOCTYPE html>
+<html lang="en">
+    <head>
+            <meta charset="utf-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+                        <title>Neptune Admin Dashboard 1 | Neptune - Multipurpose Bootstrap Admin Dashboard Template </title>
+                            <link rel="icon" type="image/x-icon" href="common-assets/img/favicon.ico"/>
+                                <!-- Common Styles Starts -->
+                                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+                                        <link href="common-assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+                                            <link href="assets/css/main.css" rel="stylesheet" type="text/css" />
+                                                <link href="assets/css/structure.css" rel="stylesheet" type="text/css" />
+                                                    <link href="common-assets/plugins/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" type="text/css" />
+                                                        <link href="common-assets/plugins/highlight/styles/monokai-sublime.css" rel="stylesheet" type="text/css" />
+                                                            <!-- Common Styles Ends -->
+                                                                <!-- Common Icon Starts -->
+                                                                    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+                                                                        <!-- Common Icon Ends -->
+                                                                            <!-- Page Level Plugin/Style Starts -->
+                                                                                <link href="assets/css/loader.css" rel="stylesheet" type="text/css" />  
+                                                                                    <link href="common-assets/plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
+                                                                                    
