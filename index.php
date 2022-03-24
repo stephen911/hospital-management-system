8aea76df3faffefd184daa00065316ac97f611d0
stephen911 <stephendappah1@gmail.com>
2022-03-24 20:16:09 +0000

    'new'

--- index.php
+++ index.php
@@ -6,4 +6,6 @@
                     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
                         <title>Neptune Admin Dashboard 1 | Neptune - Multipurpose Bootstrap Admin Dashboard Template </title>
                             <link rel="icon" type="image/x-icon" href="common-assets/img/favicon.ico"/>
-                            
+                                <!-- Common Styles Starts -->
+                                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+                                    
