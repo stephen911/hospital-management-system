ec2f2e8d19bc2351f3b68128d9dd10d0d957659f
stephen911 <stephendappah1@gmail.com>
2022-03-24 20:50:25 +0000

    'new'

--- index.php
+++ index.php
@@ -22,4 +22,6 @@
                                                                                     <link href="common-assets/plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
                                                                                         <link href="assets/css/dashboard/dashboard_1.css" rel="stylesheet" type="text/css" />
                                                                                             <link href="common-assets/plugins/flatpickr/flatpickr.css" rel="stylesheet" type="text/css">
-                                                                                            
+                                                                                                <link href="common-assets/plugins/flatpickr/custom-flatpickr.css" rel="stylesheet" type="text/css">
+                                                                                                    <link href="assets/css/elements/tooltip.css" rel="stylesheet" type="text/css" />
+                                                                                                    
