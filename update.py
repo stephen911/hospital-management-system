1c570c7c813367ecbfd917f52bb8ff329d89d3ae
stephen911 <stephendappah1@gmail.com>
2022-03-24 04:44:11 +0000

    'new'

--- update.py
+++ update.py
@@ -26,4 +26,5 @@ def create_dataset_folders(dataset_path,labels):
                                                                                                         (h, w) = frame.shape[:2]
                                                                                                                 blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),(104.0, 177.0, 123.0))
                                                                                                                 
-                                                                                                                "
+                                                                                                                        ~ pass the blob through the network and obtain the face detections
+                                                                                                                        "
