d19adfd6b42f43d909345e95f6791d94b76ef376
stephen911 <stephendappah1@gmail.com>
2022-03-24 04:10:22 +0000

    'new'

--- display.py
+++ display.py
@@ -42,7 +42,8 @@ def create_dataset_folders(dataset_path,labels):
                                                                                                 ~ initialize our list of faces, their corresponding locations,
                                                                                                     ~ and the list of predictions from our face mask network
                                                                                                         locs = []
-                                                                                                        
+                                                                                                            ~ loop over the detections
+                                                                                                            
 
 
 
