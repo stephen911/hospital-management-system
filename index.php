e43eb4c242af5ed100be09ad056b44471ecb3380
stephen911 <stephendappah1@gmail.com>
2022-03-24 18:24:32 +0000

    'new'

--- index.php
+++ index.php
@@ -415,3 +415,5 @@
                                                                                                         <a data-toggle="collapse" href="index.html~authTypeOne" role="button" aria-expanded="false" aria-controls="collapseExample" class="dropdown-toggle">
                                                                                                                                                 Type 1 <i class="las la-angle-right sidemenu-right-icon"></i>
 </a>
+                                    <ul class="sub-submenu-list collapse" id="authTypeOne"> 
+                                        
