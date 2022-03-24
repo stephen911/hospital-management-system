67ba95ba92264d94a8d7a0656dc16428b491f634
stephen911 <stephendappah1@gmail.com>
2022-03-24 04:42:51 +0000

    'new'

--- update.py
+++ update.py
@@ -15,4 +15,5 @@ import time
 
 def create_dataset_folders(dataset_path,labels):
             for label in labels:
-                    
+                            dataset_folder = dataset_path+"\\"+label
+                            "
