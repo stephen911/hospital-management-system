a1de86199cbb89225d01105dbcc6cccd7a6965cc
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:14:10 +0000

    'new'

--- display.py
+++ display.py
@@ -83,6 +83,7 @@
 ~ times, the program stores a table of values in a dictionary where the keys are
 ~ the positions. This way, repeated positions can have their evaluations looked up
 ~ fairly quickly, as the board state is hashed.
+~ 3. Opening Book - The opening book is again a dictionary that stores board
 
 
 
