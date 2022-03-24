9086ae638d9a51a512c7cd580da9cbcfd814d6fc
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:11:30 +0000

    'new'

--- display.py
+++ display.py
@@ -61,6 +61,7 @@
 ~ The GUI takes the current state and displays it on the screen. The GUI allows
 ~ drag and drop movement of pieces as well as click-click movement.
 ~
+~ The AI that plays against the human evaluates all possible moves made by either
 
 
 
