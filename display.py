a100f00bf4da95d648dea7df456d689d4f239b0c
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:25:43 +0000

    'new'

--- display.py
+++ display.py
@@ -178,6 +178,7 @@ import threading ~To allow for AI to think simultaneously while the GUI is color
 ~ stores a reference to the image that the instance of the class should display when needed. It
 ~ also stores the coordinates at which the shade would be applied.
 ~ 3. Piece - This is also used for GUI. A Piece object stores the information about the image
+~ that a piece should display (pawn, queen, etc.) and the coordinate at which it should be
 )
 
 
