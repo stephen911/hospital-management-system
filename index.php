4875ba49a0c6d23bf9beb55b5b9ac355c6a6ee35
stephen911 <stephendappah1@gmail.com>
2022-03-24 20:51:31 +0000

    'new'

--- index.php
+++ index.php
@@ -30,4 +30,6 @@
         <!-- Loader Starts -->
             <div id="load_screen"> 
                         <div class="boxes">
-                            
+                                        <div class="box">
+                                                            <div></div><div></div><div></div><div></div>
+</div>
