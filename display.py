6a12e504653b9a2b1197c4cc4e98ba0a603faf02
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:16:51 +0000

    'new'

--- display.py
+++ display.py
@@ -105,6 +105,7 @@
 ~        side has a set of pawns with the above features their points are slightly lower
 ~        to indicate a slight disadvantage in such a position.
 ~    d) A checkmate: a position where this has occured gets a very high point, so that the
+~        AI moves towards this if it can. (or avoids it).
 
 
 
