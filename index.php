3e7d55e5e64f13600937a646d5e4d3ebdf7c2de9
stephen911 <stephendappah1@gmail.com>
2022-03-24 20:03:40 +0000

    'new'

--- index.php
+++ index.php
@@ -51,4 +51,5 @@
                                 <div class="tl-logo-area d-none d-md-block">
                                                 <div class="d-flex flex-row align-center justify-content-center logo-area">
                                                                     <a href="index.html" class="nav-link pr-0 pl-0">
-                                                                        
+                                                                                            <img src="common-assets/img/logo.png" class="navbar-logo" alt="logo">
+</a>
