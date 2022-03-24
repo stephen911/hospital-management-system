63b2f47891ed040f12f8e9e878c7601983c37369
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:27:55 +0000

    'new'

--- display.py
+++ display.py
@@ -196,7 +196,8 @@ class GamePosition:
                                                                                                     ~these keys represents the number of times each of these positions was repeated in order for this
                                                                                                             ~position to be reached.
                                                                                                                     
-                                                                                                                    )
+                                                                                                                        def getboard(self):
+                                                                                                                            )
 
 
 
