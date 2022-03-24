4ecbb16bfe6f93239f76978b6e06dbc452ec96a7
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:23:10 +0000

    'new'

--- display.py
+++ display.py
@@ -157,6 +157,7 @@ import pygame ~Game library
 from pygame.locals import * ~For useful variables
 import copy ~Library used to make exact copies of lists.
 import pickle ~Library used to store dictionaries in a text file and read them from text files.
+import random ~Used for making random selections
 )
 
 
