76161f76236cb5c6a505ca4da6028141e3a99f81
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:23:47 +0000

    'new'

--- display.py
+++ display.py
@@ -162,6 +162,7 @@ from collections import defaultdict ~Used for giving dictionary values default d
 from collections import Counter ~For counting elements in a list effieciently.
 import threading ~To allow for AI to think simultaneously while the GUI is coloring the board.
 
+
 )
 
 
