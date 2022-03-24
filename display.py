52b4fc9958b31a80a0303e84710a035348098529
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:25:21 +0000

    'new'

--- display.py
+++ display.py
@@ -175,6 +175,7 @@ import threading ~To allow for AI to think simultaneously while the GUI is color
 ~ 2. Shades - This is used for GUI. A shade is a transparent colored image that is displayed on
 ~ a specific square of the chess board, in order to show various things to the user such as
 ~ the squares to which a piece may move, the square that is currently selected, etc. The class
+~ stores a reference to the image that the instance of the class should display when needed. It
 )
 
 
