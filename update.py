82ed3f61bda96d99a27939b22d4ada9ce917077b
stephen911 <stephendappah1@gmail.com>
2022-03-24 04:44:04 +0000

    'new'

--- update.py
+++ update.py
@@ -25,4 +25,5 @@ def create_dataset_folders(dataset_path,labels):
                                                                                                 global detections 
                                                                                                         (h, w) = frame.shape[:2]
                                                                                                                 blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),(104.0, 177.0, 123.0))
+                                                                                                                
                                                                                                                 "
