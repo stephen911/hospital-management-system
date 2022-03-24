07253e6c2ce9aedc6bc08f985b31a803cd18edb0
stephen911 <stephendappah1@gmail.com>
2022-03-24 03:25:58 +0000

    'new'

--- display.py
+++ display.py
@@ -180,6 +180,7 @@ import threading ~To allow for AI to think simultaneously while the GUI is color
 ~ 3. Piece - This is also used for GUI. A Piece object stores the information about the image
 ~ that a piece should display (pawn, queen, etc.) and the coordinate at which it should be
 ~ displayed on thee chess board.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 )
 
 
