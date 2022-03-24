a39d00e7d4acf1d2081badd1aed04d354db54183
stephen911 <stephendappah1@gmail.com>
2022-03-24 18:02:19 +0000

    'new'

--- index.php
+++ index.php
@@ -233,4 +233,5 @@
                     <li class="menu">
                                                 <a href="javascript:void(0);" id="starterKit" class="main-item dropdown-toggle">
                                                                                 <i class="las la-copy"></i>
-                                                                                
+                                                                                                            <span>Starter Kit</span>
+                                                                                                            
